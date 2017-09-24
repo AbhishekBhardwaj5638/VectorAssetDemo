824eb19d51dd71609ef22b1c5ca45dd31c5722e7
Abhishek Bhardwaj <abhishekbhardwaj5638@gmail.com>
2017-09-23 22:50:05 -0700

    Modification in Main Activity  and Added Image Programmatically

--- app/src/main/java/com/developer/abhi/vectorassetdemo/MainActivity.java
+++ app/src/main/java/com/developer/abhi/vectorassetdemo/MainActivity.java
@@ -2,12 +2,17 @@ package com.developer.abhi.vectorassetdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageProgrammatically;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageProgrammatically=(ImageView)findViewById(R.id.imagePragmatically);
+        imageProgrammatically.setImageResource(R.drawable.ic_svg);
     }
 }
